caf42cdafd3add17a2f997233a6da1dfcf461b1f
Bruno Harlis <harlisbruno@gmail.com>
2021-12-06 19:53:59 -0300

    up

--- scripts/insercao.py
+++ scripts/insercao.py
@@ -15,7 +15,7 @@ func_ferias = spark.read.csv("hdfs:///tmp/data/exploracaodados/funcionario_feria
 func_logs = spark.read.csv("hdfs:///tmp/data/exploracaodados/funcionario_logs.csv", header=True, inferSchema=True)
 maq_producao = spark.read.csv("hdfs:///tmp/data/exploracaodados/maquina_producao.csv", header=True, inferSchema=True)
 maq_tempo_ativa = spark.read.csv("hdfs:///tmp/data/exploracaodados/maquina_tempo_ativa.csv", header=True, inferSchema=True)
-fabrica_receita = spark.read.csv("hdfs:///tmp/data/exploracaodados/fabrica_receita.csv", header=True, inferSchema=true)
+fabrica_receita = spark.read.csv("hdfs:///tmp/data/exploracaodados/fabrica_receita.csv", header=True, inferSchema=True)
 fabrica_ambiente = spark.read.csv("hdfs:///tmp/data/exploracaodados/fabrica_ambiente.csv", header=True, inferSchema=True)
 
  
@@ -45,7 +45,7 @@ func_dados.write.mode("overwrite").saveAsTable("rh.funcionario", format="parquet
 #*************************************************
 # CRIAR E INSERIR TABELA RH.FABRICA
 #*************************************************
-fabrica_id = func_dados.filter(col("fabrica_id").distinct().sort("fabrica_id")
+fabrica_id = func_dados.select("fabrica_id").distinct().sort("fabrica_id")
 fabrica_id.write.mode("overwrite").saveAsTable("rh.fabrica", format="parquet")
 
 
@@ -66,8 +66,8 @@ tempo_licenca.write.mode("overwrite").saveAsTable("rh.tempo_licenca", format="pa
 #*************************************************
 # CRIAR E INSERIR TABELA RH.TEMPO_TRABALHADO
 #*************************************************
-tempo_trabalhado = funncionario_logs.withColumnRenamed("data", "dia_trabalhado")
-tempo_trabalhado.write.mode("overwrite").saveAsTable("tempo_trabalhado", format="parquet")
+tempo_trabalhado = func_logs.withColumnRenamed("data", "dia_trabalhado")
+tempo_trabalhado.write.mode("overwrite").saveAsTable("rh.tempo_trabalhado", format="parquet")
 
 
 #*************************************************
@@ -79,16 +79,16 @@ maq_producao.write.mode("overwrite").saveAsTable("fabrica.maq_producao", format=
 #*************************************************
 # CRIAR E INSERIR TABELA FABRICA.MAQ_TEMPO_ATIVA
 #*************************************************
-maq_tempo_atica.write.mode("overwrite").saveAsTable("fabrica.maq_temp_ativa", format="parquet")
+maq_tempo_ativa.write.mode("overwrite").saveAsTable("fabrica.maq_temp_ativa", format="parquet")
 
 
 #*************************************************
 # CRIAR E INSERIR TABELA FABRICA.MAQ_RECEITA
 #*************************************************
-fabrica_receita.write.mode("overwrite").saveAsTable("fabrica.maq_receita", format="parquent")
+fabrica_receita.write.mode("overwrite").saveAsTable("fabrica.maq_receita", format="parquet")
 
 
 #*************************************************
 # CRIAR E INSERIR TABELA FABRICA.DADOS_AMBIENTE
 #*************************************************
-fabrica_ambiente.write.mode("overwrite")saveAsTable("fabrica.dados_ambiente", format="parquet")
+fabrica_ambiente.write.mode("overwrite").saveAsTable("fabrica.dados_ambiente", format="parquet")
